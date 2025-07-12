724aba782891d0f830d392395b763bce52fdf7fc
Nilton Ramos <niltonencarnacion17@gmail.com>
2025-07-12 12:23:00 -0500

    first commit

---
+++ src/main/java/com/sistemas/TutoriaconsejeriaUnsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoriaconsejeriaUnsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoriaconsejeriaUnsBackendApplication.class, args);
+	}
+
+}

541e721f1945a95f95047d92959a470c338144e4
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-19 20:55:37 -0500

    feat(domain/InstructorSchedule): Add InstructorSchedule entity with relationships to AcademicSchedule and Instructor

--- src/main/java/com/sistemas/domain/StudentSchedule.java
+++ src/main/java/com/sistemas/domain/StudentSchedule.java
@@ -1,4 +1,40 @@
 package com.sistemas.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "student_schedules")
 public class StudentSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "instructor_schedule_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "schedule_id",
+            referencedColumnName = "schedule_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_student_schedules_academic_schedules"))
+    private AcademicSchedule academicSchedule;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "student_id",
+            referencedColumnName = "student_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_student_schedules_students"))
+    private Student student;
+
+    @Column(nullable = false)
+    @Min(value = 1, message = "La condición de matrícula debe ser al menos 1")
+    @Max(value = 3, message = "La condición de matrícula debe ser como máximo 3")
+    @NotBlank(message = "El campo condición de matrícula no debe quedar vacío")
+    private Integer registrationCondition;
 }

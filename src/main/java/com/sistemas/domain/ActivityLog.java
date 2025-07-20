a9d638dd17d1479c1e50b676741d0bf0d0247b6f
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-19 20:26:42 -0500

    feat(domain/ActivityLog): Add ActivityLog entity with relationships and fields

--- src/main/java/com/sistemas/domain/ActivityLog.java
+++ src/main/java/com/sistemas/domain/ActivityLog.java
@@ -1,4 +1,32 @@
 package com.sistemas.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "activity_logs")
 public class ActivityLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "activity_log_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "appointment_id",
+            referencedColumnName = "appointment_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_activity_logs_appointments"))
+    private Appointment appointment;
+
+    private String orientation;
+
+    private String referralToArea;
+
+    private String observation;
+
+    @Column(length = 7, nullable = false)
+    private String semester;
 }

8bebd8558e718661a9729739522f4694f32c9092
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-19 19:49:37 -0500

    feat(domain/Appointment): Add Appointment class

--- src/main/java/com/sistemas/domain/Appointment.java
+++ src/main/java/com/sistemas/domain/Appointment.java
@@ -1,4 +1,95 @@
 package com.sistemas.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "appointments")
 public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "appointment_id")
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    @Future(message = "Ingrese una fecha válida")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime endTime;
+
+    @Column(name = "modality", length = 1)
+    private char appointmentModalityCode;
+
+    @Transient
+    private AppointmentModality appointmentModality;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "reason", nullable = false, length = 1)
+    private AppointmentReason appointmentReason;
+
+    @Column(name = "specific_reason", length = 100)
+    @Size(max = 100, message = "El motivo específico no debe superar los 100 caracteres")
+    private String specificAppointmentReason;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "method", nullable = false, length = 1)
+    private AppointmentMethod appointmentMethod;
+
+    @Column(name = "specific_method", length = 100)
+    @Size(max = 100, message = "La forma de cita específica no debe superar los 100 caracteres")
+    private String specificAppointmentMethod;
+
+    @Column(name = "state", length = 1)
+    private char appointmentStateCode;
+
+    @Transient
+    private AppointmentState appointmentState;
+
+    @Column(name = "type_activity", length = 1)
+    private char typeActivityCode;
+
+    @Transient
+    private TypeActivity typeActivity;
+
+    public AppointmentModality getAppointmentModality(){
+        return AppointmentModality.fromCode(appointmentModalityCode);
+    }
+
+    public void setAppointmentModality(AppointmentModality appointmentModality){
+        this.appointmentModality = appointmentModality;
+        this.appointmentModalityCode = appointmentModality.getCode();
+    }
+
+    public AppointmentState getAppointmentState(){
+        return AppointmentState.fromCode(appointmentStateCode);
+    }
+
+    public void setAppointmentState(AppointmentState appointmentState){
+        this.appointmentState = appointmentState;
+        this.appointmentStateCode = appointmentState.getCode();
+    }
+
+    public TypeActivity getTypeActivity(){
+        return TypeActivity.fromCode(typeActivityCode);
+    }
+
+    public void setTypeActivity(TypeActivity typeActivity){
+        this.typeActivity = typeActivity;
+        this.typeActivityCode = typeActivity.getCode();
+    }
 }

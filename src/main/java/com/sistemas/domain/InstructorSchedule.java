00d07fc7d2fa92f08b916cf7eeac4240a10ae9be
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-19 20:55:30 -0500

    feat(domain/InstructorSchedule): Add InstructorSchedule entity with relationships to AcademicSchedule and Instructor

--- src/main/java/com/sistemas/domain/InstructorSchedule.java
+++ src/main/java/com/sistemas/domain/InstructorSchedule.java
@@ -1,4 +1,31 @@
 package com.sistemas.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "instructor_schedules")
 public class InstructorSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "instructor_schedule_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "schedule_id",
+            referencedColumnName = "schedule_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_instructor_schedules_academic_schedules"))
+    private AcademicSchedule academicSchedule;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "instructor_id",
+            referencedColumnName = "instructor_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_instructor_schedules_instructors"))
+    private Instructor instructor;
 }

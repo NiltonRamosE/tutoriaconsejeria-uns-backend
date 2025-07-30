e832ff7d211da60ec8d8038dee15410fd1732750
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-30 11:39:39 -0500

    chore(domain/administrator): add builder annotation

--- src/main/java/com/sistemas/domain/Administrator.java
+++ src/main/java/com/sistemas/domain/Administrator.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
+@Builder
 @Table(name = "administrators")
 public class Administrator {
 

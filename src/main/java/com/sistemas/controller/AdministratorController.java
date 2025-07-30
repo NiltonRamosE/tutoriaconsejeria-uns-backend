e14fa9b82544237d3d5e7cd9534f3ca83f9a35f4
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-30 11:40:12 -0500

    chore(domain/administrator): remove password encoder

--- src/main/java/com/sistemas/controller/AdministratorController.java
+++ src/main/java/com/sistemas/controller/AdministratorController.java
@@ -5,7 +5,6 @@ import com.sistemas.service.AdministratorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +17,9 @@ public class AdministratorController {
     @Autowired
     private AdministratorService administratorService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/create")
     public ResponseEntity<Administrator> createAdministrator(@Valid @RequestBody Administrator administrator) {
-
-        administrator.setPassword(passwordEncoder.encode(administrator.getPassword()));
-
         Administrator savedAdministrator = administratorService.create(administrator);
         return ResponseEntity.ok(savedAdministrator);
     }
--- src/main/java/com/sistemas/controller/StudentController.java
+++ src/main/java/com/sistemas/controller/StudentController.java
@@ -5,7 +5,6 @@ import com.sistemas.service.StudentService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +17,8 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @PostMapping("/create")
     public ResponseEntity<Student> createStudent(@Valid @RequestBody Student student) {
-
-        student.setPassword(passwordEncoder.encode(student.getPassword()));
-
         Student savedStudent = studentService.create(student);
         return ResponseEntity.ok(savedStudent);
     }

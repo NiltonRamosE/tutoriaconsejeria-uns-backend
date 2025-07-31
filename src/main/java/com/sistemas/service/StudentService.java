718669c9f91baa31d0b83c810279b8a5833d7f86
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-31 01:59:01 -0500

    feat(service): add abstract methods

--- src/main/java/com/sistemas/service/StudentService.java
+++ src/main/java/com/sistemas/service/StudentService.java
@@ -2,9 +2,18 @@ package com.sistemas.service;
 
 import com.sistemas.domain.Student;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentService extends IGenericService<Student, Long> {
 
     Optional<Student> findByInstitutionalEmail(String institutionalEmail);
+
+    List<Student> getStudentsEarlyStage();
+
+    List<Student> getStudentsLateStage();
+
+    List<Student> getIrregularConditionStudentsEarlyStage();
+
+    List<Student> getIrregularConditionStudentsLateStage();
 }

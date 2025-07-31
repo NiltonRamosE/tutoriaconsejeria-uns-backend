c424d2938518b379eef9de0f582ccbf775db93dd
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-31 02:00:43 -0500

    feat(repository): add repository class

---
+++ src/main/java/com/sistemas/repository/AcademicAssignmentRepository.java
@@ -0,0 +1,44 @@
+package com.sistemas.repository;
+
+import com.sistemas.domain.AcademicAssignment;
+import com.sistemas.domain.Instructor;
+import com.sistemas.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AcademicAssignmentRepository extends JpaRepository<AcademicAssignment, Long> {
+
+    @Query("SELECT COUNT(aa) FROM AcademicAssignment aa WHERE aa.instructor.id = :instructorId AND aa.typeActivityCode = :typeActivityCode")
+    long countStudentsByInstructorAndTypeActivity(
+            @Param("instructorId") Long instructorId,
+            @Param("typeActivityCode") char typeActivityCode
+    );
+
+    @Query(value = """
+        SELECT i
+        FROM AcademicAssignment aa
+        INNER JOIN aa.instructor i
+        WHERE aa.student.id = :studentId
+          AND aa.typeActivityCode = :typeActivityCode
+    """)
+    Instructor findInstructorsByStudentId(
+            @Param("studentId") Long studentId,
+            @Param("typeActivityCode") char typeActivityCode
+    );
+
+
+    @Query(value = """
+        SELECT s
+        FROM AcademicAssignment aa
+        INNER JOIN aa.student s
+        WHERE aa.instructor.id = :instructorId
+          AND aa.typeActivityCode = :typeActivityCode
+    """)
+    List<Student> findStudentsByInstructorId(
+            @Param("instructorId") Long instructorId,
+            @Param("typeActivityCode") char typeActivityCode
+    );
+}

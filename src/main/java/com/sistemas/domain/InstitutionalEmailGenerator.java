7bdc9702fc5ed7985882cd6601df425775a86ff5
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-08-19 07:44:35 -0500

    feat: enhance institutional email generation by removing accents from names

--- src/main/java/com/sistemas/domain/InstitutionalEmailGenerator.java
+++ src/main/java/com/sistemas/domain/InstitutionalEmailGenerator.java
@@ -1,5 +1,7 @@
 package com.sistemas.domain;
 
+import java.text.Normalizer;
+
 public class InstitutionalEmailGenerator {
     /**
      * Generates an institutional email address based on the provided name and surnames.
@@ -11,16 +13,29 @@ public class InstitutionalEmailGenerator {
      */
 
     public static String generateInstitutionalEmail(String name, String paternalSurname, String maternalSurname) {
+        name = removeAccents(name);
+        paternalSurname = removeAccents(paternalSurname);
+        maternalSurname = removeAccents(maternalSurname);
+
         String[] names = name.split(" ");
         StringBuilder emailBuilder = new StringBuilder();
 
         for (String part : names) {
-            emailBuilder.append(part.charAt(0));
+            if (!part.isEmpty()) {
+                emailBuilder.append(part.charAt(0));
+            }
         }
 
         emailBuilder.append(paternalSurname);
-        emailBuilder.append(maternalSurname.charAt(0));
+        if (!maternalSurname.isEmpty()) {
+            emailBuilder.append(maternalSurname.charAt(0));
+        }
 
         return emailBuilder.toString().toLowerCase() + "@uns.edu.pe";
+    }
+
+    private static String removeAccents(String input) {
+        String normalized = Normalizer.normalize(input, Normalizer.Form.NFD);
+        return normalized.replaceAll("\\p{M}", ""); // \p{M} es marca de acento en Unicode
     }
 }

114b8517421d2cbf6c2f7476ce9aaf1b91ff463a
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-08-24 17:38:59 -0500

    chore: delete interface AppointmentRequestWithAltSchedule.java

--- src/main/java/com/sistemas/dto/appointment_schedule/AppointmentRequestWithAltSchedule.java
+++
@@ -1,7 +0,0 @@
-package com.sistemas.dto.appointment_schedule;
-
-public interface AppointmentRequestWithAltSchedule {
-    String getAltScheduleA();
-    String getAltScheduleB();
-    String getAltScheduleC();
-}

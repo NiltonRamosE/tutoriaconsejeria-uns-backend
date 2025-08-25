41cd589410736575dd08ef667ddbe2dc3bd9aef1
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-08-25 11:54:08 -0500

    feat: add appointment create endpoints by instructor

--- src/main/java/com/sistemas/controller/InstructorController.java
+++ src/main/java/com/sistemas/controller/InstructorController.java
@@ -1,14 +1,24 @@
 package com.sistemas.controller;
 
+import com.sistemas.domain.AcademicAssignment;
+import com.sistemas.domain.AppointmentSchedule;
 import com.sistemas.domain.Instructor;
-import com.sistemas.service.InstructorService;
+import com.sistemas.dto.appointment_schedule.ScheduleGroupAppointmentRequest;
+import com.sistemas.dto.appointment_schedule.ScheduleIndividualAppointmentRequest;
+import com.sistemas.dto.student.AssignedStudentResponse;
+import com.sistemas.mapper.AcademicAssignmentMapper;
+import com.sistemas.mapper.AppointmentMapper;
+import com.sistemas.mapper.AppointmentScheduleMapper;
+import com.sistemas.mapper.StudentMapper;
+import com.sistemas.service.*;
+import com.sistemas.service.implement.AppointmentFacadeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/instructor", produces = "application/json")
@@ -17,9 +27,47 @@ public class InstructorController {
     @Autowired
     private InstructorService instructorService;
 
+    @Autowired
+    private AppointmentFacadeService appointmentFacadeService;
+
+    @Autowired
+    private AcademicAssignmentService academicAssignmentService;
+
+    @Autowired
+    private AcademicAssignmentMapper academicAssignmentMapper;
+
     @PostMapping("")
     public ResponseEntity<Instructor> createInstructor(@Valid @RequestBody Instructor instructor) {
         Instructor savedInstructor = instructorService.create(instructor);
         return ResponseEntity.ok(savedInstructor);
     }
+
+    @GetMapping("/assigned/{id}")
+    public ResponseEntity<List<AssignedStudentResponse>> getStudentsAssignedByInstructor(@PathVariable("id") Long id) {
+        List<AcademicAssignment> academicAssignments  = academicAssignmentService.findAcademicAssignmentsByInstructorId(id);
+
+        List<AssignedStudentResponse> assignedStudentResponses = academicAssignments.stream()
+                .map(academicAssignmentMapper::mapToAssignedStudentResponse)
+                .toList();
+
+        return new ResponseEntity<>(assignedStudentResponses, HttpStatus.OK);
+    }
+
+    @PostMapping("/appointment/individual")
+    public ResponseEntity<AppointmentSchedule> scheduleIndividualAppointment(@Valid @RequestBody ScheduleIndividualAppointmentRequest request) {
+
+        AppointmentSchedule appointmentSchedule = appointmentFacadeService.createIndividualAppointment(request, "instructor");
+
+        return ResponseEntity.ok(appointmentSchedule);
+    }
+
+    @PostMapping("/appointment/group")
+    public ResponseEntity<List<AppointmentSchedule>> scheduleGroupAppointment(@Valid @RequestBody ScheduleGroupAppointmentRequest request) {
+
+        List<AppointmentSchedule> appointmentScheduleList = appointmentFacadeService.createGroupAppointment(request, "instructor");
+
+        return ResponseEntity.ok(appointmentScheduleList);
+    }
+
+
 }
